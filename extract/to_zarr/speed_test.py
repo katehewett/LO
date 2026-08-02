ffe0e77cb3b376b956a7bc9a30a669665dff4b6b
Parker MacCready <p.maccready@gmail.com>
2026-08-02 14:52:19 -0700

    Update speed_test.py

--- extract/to_zarr/speed_test.py
+++ extract/to_zarr/speed_test.py
@@ -13,7 +13,7 @@ local = False
 
 if local:
     # local version
-    fn0 = Ldir['roms_out'] / 'cas7_t2_x11b' / 'f2026.05.01' / 'ocean_his_0001.nc'
+    fn0 = Ldir['roms_out'] / 'cas7_t2_x11b' / 'f2026.05.01' / 'ocean_his_0002.nc'
     fn1 = '/gscratch/macc/parker/tmp/h_01.zarr'
 else:
     # s3 version with direct access
@@ -35,13 +35,13 @@ a = ds.salt[0,:,10,10].values
 ds.close()
 print('netcdf time = %0.3f' % (time()-tt0))
 
-tt0 = time()
-if local:
-    ds = xr.open_zarr(fn1)
-else:
-    ds = xr.open_zarr(fn1, storage_options=storage_options)
-    #ds = xr.open_zarr(fn1alt)
-a = ds.salt[0,:,10,10].values
-b = a*a
-ds.close()
-print('zarr time = %0.3f' % (time()-tt0))
+# tt0 = time()
+# if local:
+#     ds = xr.open_zarr(fn1)
+# else:
+#     ds = xr.open_zarr(fn1, storage_options=storage_options)
+#     #ds = xr.open_zarr(fn1alt)
+# a = ds.salt[0,:,10,10].values
+# b = a*a
+# ds.close()
+# print('zarr time = %0.3f' % (time()-tt0))
