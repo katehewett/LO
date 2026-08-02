a4cf22793e8fab18a93425450eab2a4aa141b307
Parker MacCready <p.maccready@gmail.com>
2026-08-02 14:49:41 -0700

    Update speed_test.py

--- extract/to_zarr/speed_test.py
+++ extract/to_zarr/speed_test.py
@@ -28,19 +28,20 @@ if local:
     ds = xr.open_dataset(fn0)
 else:
     ds = xr.open_dataset(fn0, engine='h5netcdf', storage_options=storage_options)
+
     
 a = ds.salt[0,:,10,10].values
 #b = a*a
 ds.close()
 print('netcdf time = %0.3f' % (time()-tt0))
 
-# tt0 = time()
-# if local:
-#     ds = xr.open_zarr(fn1)
-# else:
-#     #ds = xr.open_zarr(fn1, storage_options=storage_options)
-#     ds = xr.open_zarr(fn1alt)
-# a = ds.salt[0,:,10,10].values
-# b = a*a
-# ds.close()
-# print('zarr time = %0.3f' % (time()-tt0))
+tt0 = time()
+if local:
+    ds = xr.open_zarr(fn1)
+else:
+    #ds = xr.open_zarr(fn1, storage_options=storage_options)
+    ds = xr.open_zarr(fn1alt)
+a = ds.salt[0,:,10,10].values
+b = a*a
+ds.close()
+print('zarr time = %0.3f' % (time()-tt0))
