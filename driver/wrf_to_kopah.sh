76a8255fefc543b0482542e38f6ad3d830e0bb55
katehewett <hewett.kate@gmail.com>
2026-09-01 15:54:28 -0700

    Update wrf_to_kopah.sh
    loop case to load macc keys

--- driver/wrf_to_kopah.sh
+++ driver/wrf_to_kopah.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
 # code to copy today's WRF forecast files to kopah
+# flexible for Parker and Kate to run forecast
 
-# Map your unique profile keys to standard s5cmd variables
-if [[ "$USER" == *"kmhewett"* ]]; then
-    export AWS_ACCESS_KEY_ID=$MACC_KEY
-    export AWS_SECRET_ACCESS_KEY=$MACC_SECRET
-else
-    export AWS_ACCESS_KEY_ID=$access_key
-    export AWS_SECRET_ACCESS_KEY=$secret_key
+if [ -f "$HOME/.bashrc" ]; then
+    source "$HOME/.bashrc"
 fi
-export AWS_REGION='us-west-2' 
 
-# 2. Run a quick check to make sure they mapped correctly
-LOG_FILE="/gscratch/macc/kmhewett/LO/driver/wrf_to_kopah_map.log"
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-    echo "ERROR: Script subshell could not read keys from environment." > "$LOG_FILE"
-    exit 1
+CURRENT_USER=$(whoami)
+
+# UW Kopah s5cmd relies on S3_endpoint_url so this should work
+export S3_ENDPOINT_URL="$S3_ENDPOINT_URL"
+
+if [[ "$CURRENT_USER" == *"kmhewett"* ]]; then
+    load_macc
 fi
 
 dstr=`date -u +%Y%m%d`00
-indir0=/gscratch/macc/kmhewett/LO_data/wrf/
+indir0=/gscratch/macc/$CURRENT_USER/LO_data/wrf/
 indir=$indir0$dstr/
-echo $indir > /gscratch/macc/kmhewett/LO/driver/wrf_to_kopah.log
+echo $indir > /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log
 s5cmd_bin=$(command -v s5cmd || echo "/usr/local/bin/s5cmd")
-$s5cmd_bin sync $indir s3://liveocean-kmhewett/LO_data/wrf/$dstr/ >> /gscratch/macc/kmhewett/LO/driver/wrf_to_kopah.log
+
+if [[ "$CURRENT_USER" == *"kmhewett"* ]]; then
+    $s5cmd_bin sync $indir s3://liveocean-kmhewett/LO_data/wrf/$dstr/ >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log
+elif [[ "$CURRENT_USER" == *"parker"* ]]; then
+    $s5cmd_bin sync $indir s3://liveocean-pmacc/LO_data/wrf/$dstr/ >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log
+else 
+    echo "ERROR: NO MATCHING USER FOUND ($CURRENT_USER). EXITING Script." >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log
+    exit 1
+fi
+
+
