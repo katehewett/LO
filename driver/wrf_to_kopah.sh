c1095b7cd6e3191057caa89e361ba5f9c37e5f42
katehewett <hewett.kate@gmail.com>
2026-09-02 18:57:20 -0700

    edits to crontab

--- driver/wrf_to_kopah.sh
+++ driver/wrf_to_kopah.sh
@@ -3,22 +3,21 @@
 # code to copy today's WRF forecast files to kopah
 # flexible for Parker and Kate to run forecast
 
-if [ -f "$HOME/.bashrc" ]; then
-    source "$HOME/.bashrc"
-fi
+if [ -f "$HOME/.bash_profile" ]; then source "$HOME/.bash_profile"; fi
+if [ -f "$HOME/.bashrc" ]; then source "$HOME/.bashrc"; fi
 
 CURRENT_USER=$(whoami)
 
 # UW Kopah s5cmd relies on S3_endpoint_url so this should work
-# export S3_ENDPOINT_URL="$S3_ENDPOINT_URL"
+export S3_ENDPOINT_URL='https://s3.kopah.uw.edu'
 
 if [ "$CURRENT_USER" = "kmhewett" ] || [ "$CURRENT_USER" = "katehewett" ]; then
     # Kate has two sets of access keys, this pulls MACC from her bashrc 
     export AWS_ACCESS_KEY_ID="${MACC_KEY:-}"
     export AWS_SECRET_ACCESS_KEY="${MACC_SECRET:-}"
-    echo "--- Running with Kate's credentials (User: $CURRENT_USER) ---"
+    echo "--- Running with Kate's macc credentials (User: $CURRENT_USER) ---"
 else
-    echo "--- Running with credentials (User: $CURRENT_USER) ---"
+    echo "--- Running with macc credentials (User: $CURRENT_USER) ---"
 fi
 
 dstr=`date -u +%Y%m%d`00
@@ -28,11 +27,11 @@ echo $indir > /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log
 s5cmd_bin=$(command -v s5cmd || echo "/usr/local/bin/s5cmd")
 
 if [[ "$CURRENT_USER" == *"kmhewett"* ]]; then
-    $s5cmd_bin sync $indir s3://liveocean-kmhewett/LO_data/wrf/$dstr/ >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log
+    $s5cmd_bin sync $indir s3://liveocean-kmhewett/LO_data/wrf/$dstr/ >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log 
 elif [[ "$CURRENT_USER" == *"parker"* ]]; then
-    $s5cmd_bin sync $indir s3://liveocean-pmacc/LO_data/wrf/$dstr/ >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log
-else 
-    echo "ERROR: NO MATCHING USER FOUND ($CURRENT_USER). EXITING Script." >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log
+    $s5cmd_bin sync $indir s3://liveocean-pmacc/LO_data/wrf/$dstr/ >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log 
+else  
+    echo "ERROR: NO MATCHING USER FOUND ($CURRENT_USER). EXITING Script." >> /gscratch/macc/$CURRENT_USER/LO/driver/wrf_to_kopah.log 
     exit 1
 fi
 
