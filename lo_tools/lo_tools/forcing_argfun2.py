22a83b8315e8126546aba65df18710dd9a11facb
katehewett <hewett.kate@gmail.com>
2026-09-06 16:16:34 -0700

    updates for sending forcing to kopah
    add get_macc_s5mcd_env to Lfun, and update forcing_argfun2.py to add s5cmd and to update so will send forcing to kopah

--- lo_tools/lo_tools/forcing_argfun2.py
+++ lo_tools/lo_tools/forcing_argfun2.py
@@ -2,6 +2,7 @@
 Shared helper functions for the forcing code, especially for argument passing.
 
 """
+import os
 import argparse
 import sys
 from lo_tools import Lfun
@@ -87,17 +88,32 @@ def finale(Ldir, result_dict):
     # This will only write the output of the first day of a forecast.
     if Ldir['to_kopah']:
         tt0 = time()
-        bucket_name = 'liveocean-' + Ldir['local_user']
-        cmd_list = ['s3cmd','sync',str(out_dir)+'/',
-            's3://'+bucket_name+'/LO_output/forcing/'+Ldir['gridname']+'/f'+Ldir['date_string']+'/'+Ldir['frc']+'/',
-            '--acl-public']
-        proc = Po(cmd_list, stdout=Pi, stderr=Pi)
-        stdout, stderr = proc.communicate()
-        if len(stderr) > 0:
-            print('Copy to Kopah stderr')
-            # always print errors
-            print(stderr.decode())
-        print(' - time to copy to kopah = %d sec' % (time()-tt0))
-        sys.stdout.flush()
+
+        local_user = Ldir['local_user']
+
+        # Get Kopah access keys, exit if none
+        # calls Lfun function get_s5cmd_env
+        s5cmd_env = Lfun.get_macc_s5cmd_env(local_user)
+        if s5cmd_env is None:
+            print(f"Error: missing valid access key format for user '{local_user}'. Check your bashrc (bash_profile).")
+        elif local_user == 'BLANK':
+            print(f"Error: Missing a valid local user, did not send to kopah")
+        else:
+            print(f"s5cmd env successfully loaded for '{local_user}'")
+
+            bucket_name = 'liveocean-' + local_user
+            s5cmd_base = shutil.which('s5cmd') or '/usr/local/bin/s5cmd'              # find the binary path
+            s5cmd_bin = [s5cmd_base, '--endpoint-url', 'https://s3.kopah.uw.edu']     # bundle the endpoint to always target Kopah automatically
+            
+            cmd_list = s5cmd_bin + ['sync',str(out_dir)+'/*',
+                                    's3://'+bucket_name+'/LO_output/forcing/'+Ldir['gridname']+'/f'+Ldir['date_string']+'/'+Ldir['frc']+'/']
+            proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+            stdout, stderr = proc.communicate()
+            if len(stderr) > 0:
+                print('Copy to Kopah stderr')
+                # always print errors
+                print(stderr.decode())
+            print(' - time to copy to kopah = %d sec' % (time()-tt0))
+            sys.stdout.flush()
     
 
