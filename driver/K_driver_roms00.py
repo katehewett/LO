c144eef464d81717e496b63f3fa0029e2a486fa6
katehewett <hewett.kate@gmail.com>
2026-09-17 14:50:38 -0700

    updates to send to liveocean-pmacc
    pull to klone after parker turns off crontab

--- driver/K_driver_roms00.py
+++ driver/K_driver_roms00.py
@@ -521,7 +521,8 @@ while dt <= dt1:
             # set bucket name if pass tests 
             if (Ldir['kopah_user'] == 'pmacc'):
                 if (current_user in ('parker', 'pmacc', 'kmhewett', 'katehewett')):
-                    bucket_name = 'liveocean-pmacctest'
+                    #bucket_name = 'liveocean-pmacctest'
+                    bucket_name = 'liveocean-pmacc'
                     print(f"Sending forcing files to kopah bucket: liveocean-pmacc")
                 else: 
                     print(f"Error: can not send to liveocean-pmacc. Check kopah credentials.")
