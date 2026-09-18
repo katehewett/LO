73742039d44b29ed5f3e6be215987af2f47b32a3
katehewett <hewett.kate@gmail.com>
2026-09-18 10:38:39 -0700

    try 2: daymovie0

--- driver/driver_post1.py
+++ driver/driver_post1.py
@@ -28,10 +28,6 @@ from time import time, sleep
 
 from lo_tools import Lfun, zfun
 
-# kate added on 18 Sept 2026 (attempt to fix pandas.indexes error)
-import pandas
-sys.modules['pandas.indexes'] = pandas.core.indexes
-
 parser = argparse.ArgumentParser()
 # which run to use
 parser.add_argument('-gtx', '--gtagex', type=str)   # e.g. cas6_v3_l08b
--- driver/driver_post2.py
+++ driver/driver_post2.py
@@ -31,10 +31,6 @@ from time import time, sleep
 
 from lo_tools import Lfun, zfun
 
-# kate added on 18 Sept 2026 (attempt to fix pandas.indexes error)
-import pandas
-sys.modules['pandas.indexes'] = pandas.core.indexes
-
 parser = argparse.ArgumentParser()
 # which run to use
 parser.add_argument('-gtx', '--gtagex', type=str)   # e.g. wgh2_t0_xn0b
